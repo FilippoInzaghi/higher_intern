4e32d0f86344225214a258eab969c6b2a092ecdf
FilippoInzaghi <filipkazmierczak18@gmail.com>
2019-06-20 21:00:05 +0200

    Updates

---
+++ precache-manifest.7ca97ac1c21b7d295e511aef04fe6b65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4229506d264ee212bcbb2381dbcce40b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeacca1b9228513d3736",
+    "url": "/static/css/main.10fcaad2.chunk.css"
+  },
+  {
+    "revision": "859c4dcb3cbdf8a2563c",
+    "url": "/static/js/2.b4cbf3e9.chunk.js"
+  },
+  {
+    "revision": "eeacca1b9228513d3736",
+    "url": "/static/js/main.ec2995fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
